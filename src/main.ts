b352750c692ffb2867abfcedbf77f9f837564c29
QianYang <qianyang@foxmail.com>
2022-04-05 23:26:18 +0800

    eslint for ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import { router } from "./router";
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import { createApp } from "vue"
+import App from "./App.vue"
+import { router } from "./router"
 
-createApp(App).use(router).use(ElementPlus).mount("#app");
+createApp(App).use(router).use(ElementPlus).mount("#app")
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,9 @@
-import Home from "@/views/Home.vue";
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/views/Home.vue"
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 
-const routes: RouteRecordRaw[] = [
-  { name: "index", path: "/", component: Home },
-];
+const routes: RouteRecordRaw[] = [{ name: "index", path: "/", component: Home }]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
