c0fe67a69b73dab062eb8cda0d56b7c238de68f9
QianYang <qianyang@foxmail.com>
2022-04-05 21:00:30 +0800

    调整 tsconfig.json

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,11 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router"
-
-import Home from "@/views/Home.vue"
+import Home from "@/views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-    {name: "index", path: "/", component: Home}
-]
+  { name: "index", path: "/", component: Home },
+];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
+  history: createWebHistory(),
+  routes,
+});
